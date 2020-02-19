4bc4db7460e701052cc6cbe8ac8d75c5b6305d27
ShilovAndrey <A_Shiloff@mail.ru>
2020-02-19 22:35:57 +0300

    coacher

--- src/coacher/Coacher.java
+++ src/coacher/Coacher.java
@@ -1,5 +1,9 @@
 package coacher;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -11,51 +15,113 @@ public class Coacher {
             "рекурсия.txt", "сериализация.txt", "создание массива.txt", "выбор_задания.txt",
             "сортировка книг.txt", "подключение к JDBC.txt"};
     private String[] algorithms = {"1_OrderedArray.txt", "2_ClassDataApp.txt", "3_StackApp.txt", "4_BracketsApp.txt",
-            "5_Queue.txt", "6_PriorityQueue.txt"};
+            "5_Queue.txt", "6_PriorityQueue.txt","7_LinkList_Stack.txt","8_LinkListQueue.txt","9_LinkedList.txt",
+            "10_Recursion.txt","11_MergeSort.txt"};
     private String[] sql = {"1 create, insert.txt", "2 select,order.txt", "3 like, where IN.txt", "4 inner join.txt",
             "5 union.txt", "6 функции.txt", "7 группировка и агрегаты.txt", "8 подзапросы.txt",
             "9 внешние соединения.txt", "10 условная логика.txt"};
-    private String adressJava = "повторение_IT/задания_Java/";              // часть адреса к файлу
-    private String adressAlgorithms = "повторение_IT/задания_Algorithms/";
-    private String adressSQL = "повторение_IT/задания_sql/";
-    private String adress;
+    private String addressJava = "C:/Users/1/Desktop/ИТ/повторение_IT/задания_Java/";          // часть адреса к файлу
+    private String addressAlgorithms = "C:/Users/1/Desktop/ИТ/повторение_IT/задания_Algorithms/";
+    private String addressSQL = "C:/Users/1/Desktop/ИТ/повторение_IT/задания_sql/";
+    private String address;
     private String task;
+    private JTextArea incoming;
+    private JPanel panel;
+    private JTextField instructionField;
+    private JButton javaButton;
+    private JButton algButton;
+    private JButton sqlButton;
+    private JScrollPane scroller;
 
 
-    private String line;
 
-    public Coacher(String inputLine) {                             // конструктор
-        line = inputLine;                                          // получаем пользовательский ввод
+    public void startCoacher(){
+
+        JFrame frame = new JFrame("Coacher");
+        frame.setLocation(870,10);
+        panel = new JPanel();
+        createInstructionField();
+        createButtons();
+        javaButton.addActionListener(new JavaButtonListener());
+        algButton.addActionListener(new AlgButtonListener());
+        sqlButton.addActionListener(new SQLButtonListener());
+        createTextArea();
+        frame.getContentPane().add(BorderLayout.CENTER,panel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setSize(500, 740);
+        frame.setVisible(true);
+
     }
 
-    public void startCoacher() {
-        if (line.equalsIgnoreCase("java")) {          // обрабатываем пользовательский ввод
-            adress = adressJava;                                   //  сохраняем в переменную часть адреса,
-            // зависимости от того, какую тему хочет повторить пользователь
-            getTitleTask(java);                    // передаем ссылку на массив с заданиями по выбранной теме
-        } else if (line.equalsIgnoreCase("sql")) {
-            adress = adressSQL;
-            getTitleTask(sql);
-        } else if (line.equalsIgnoreCase("alg")) {
-            adress = adressAlgorithms;
+    private void createInstructionField() {
+
+        instructionField = new JTextField(43);
+        instructionField.setText("Выберите из какой категории будет задание:");
+        instructionField.setEditable(false);
+        panel.add(instructionField);
+    }
+
+    private void createButtons(){
+
+        javaButton = new JButton("java");
+        algButton = new JButton("algorithms");
+        sqlButton = new JButton("sql");
+        panel.add(javaButton);
+        panel.add(algButton);
+        panel.add(sqlButton);
+    }
+
+    public void createTextArea() {
+
+        incoming=new JTextArea(30,40);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        scroller = new JScrollPane(incoming);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        panel.add(scroller);
+    }
+
+    private class JavaButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e){
+            address = addressJava;
+            getTitleTask(java);
+        }
+    }
+
+    private class AlgButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e){
+            address = addressAlgorithms;
             getTitleTask(algorithms);
+        }
+    }
+
+    private class SQLButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e){
+            address = addressSQL;
+            getTitleTask(sql);
         }
     }
 
     private void getTitleTask(String[] arrayTasks) {
         int num = (int) (Math.random() * arrayTasks.length);  // получаем рандомное число
         task = arrayTasks[num];                               // используем его для выбора задачи в нашем массиве
-        System.out.println("выполни задание " + task + " ");
-        downloadTask();                                       // считываем файл и выводим задачу на экран
+        downloadTask();                                       // считываем файл и выводим задачу в incoming
     }
 
     private void downloadTask() {
-        File file = new File(adress + task);
+        incoming.setText("");
+        File file = new File(address + task);
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String string = null;
-            while ((string = reader.readLine()) != null)  // пока есть что считывать
-                System.out.println(string);
+            while ((string = reader.readLine()) != null)      // пока есть что считывать
+                incoming.append(string+"\n");
         } catch (IOException ex) {
             System.out.println(ex);
         }
